b89b1fe04c1a6a342cd77731bf2ee72662e6a446
Briana Peppers <peppersbre@gmail.com>
2024-02-22 00:28:08 -0500

    dust yo self off and try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,6 @@ import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 import react from '@astrojs/react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-// Convert the file URL to a file path
-const __filename = fileURLToPath(import.meta.url)
-// Get the directory name from the file path
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
 	site: 'https://simplysteeped.com/',
@@ -21,18 +14,6 @@ export default defineConfig({
 		shikiConfig: {
 			theme: 'material-theme-palenight',
 			wrap: true
-		}
-	},
-	vite: {
-		resolve: {
-			alias: {
-				// Define your path aliases here
-				'@/components': path.resolve(__dirname, './src/components'),
-				'@/layouts': path.resolve(__dirname, './src/layouts'),
-				'@/styles': path.resolve(__dirname, './src/styles'),
-				'@/utils': path.resolve(__dirname, './src/utils'),
-				'@/site-config': path.resolve(__dirname, './src/data/site.config.ts')
-			}
 		}
 	},
 	integrations: [
