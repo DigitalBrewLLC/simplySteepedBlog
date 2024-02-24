a0a2bad8677afe5a26627328db7368f8571adf32
Briana Peppers <peppersbre@gmail.com>
2024-02-24 16:04:17 -0500

    upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,32 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import partytown from '@astrojs/partytown'
-import { remarkReadingTime } from './src/utils/readTime.ts'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import { remarkReadingTime } from './src/utils/readTime.ts';
+import react from '@astrojs/react';
 
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://simplysteeped.com/',
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				theme: 'material-theme-palenight',
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push']
-			}
-		}),
-		react()
-	]
-})
+  site: 'https://simplysteeped.com/',
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    drafts: true,
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+      wrap: true
+    }
+  },
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+      wrap: true
+    },
+    drafts: true
+  }), sitemap(), tailwind(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), react()]
+});
